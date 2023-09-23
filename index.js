190acc55322db1d804ce344de2ef70b9da1f6693
azubair2012 <studio.zubair@gamil.com>
2023-09-23 12:55:38 +0600

    s

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const dotenv = require("dotenv").config(); // to use.env file for environment va
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const connectDb = require("./config/dbConnection");
+const postRoute = require("./routers/postRoute");
+const getRoute = require("./routers/getRoute");
+const deleteRoute = require("./routers/deleteRoute");
+const updateRoute = require("./routers/updateRoute");
 connectDb();
 
 app.all("/", (req, res) => {
     console.log("Just got a request!");
-    res.send("Hello...!");
+    res.send("Hello...4!");
 });
 
 app.listen(port || 5000);
