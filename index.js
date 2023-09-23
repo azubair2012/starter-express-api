2213ebb07d322bb3c5f1e936523880165f3c8c17
azubair2012 <studio.zubair@gamil.com>
2023-09-23 12:42:13 +0600

    s

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 const port = process.env.PORT;
 app.all("/", (req, res) => {
     console.log("Just got a request!");
-    res.send("Hello!");
+    res.send("Hello...!");
 });
 app.listen(port || 5000);
